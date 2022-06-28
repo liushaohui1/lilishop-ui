25253a30abaa7f70dead4a46ddad7dbfd184b4fd
liushaohui <liushaohui@midland.com.cn>
2022-06-28 14:47:16 +0800

    修改配置

--- manager/public/config.js
+++ manager/public/config.js
@@ -3,14 +3,10 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn",
-      //  manager: "http://192.168.0.120:8887",
-      //  common: "http://192.168.0.120:8890",
-
-
+      common: "http://127.0.0.1:8890",
+      buyer: "http://127.0.0.1:8888",
+      seller: "http://127.0.0.1:8889",
+      manager: "http://127.0.0.1:8887"
     },
     API_PROD: {
       common: "https://common-api.pickmall.cn",
